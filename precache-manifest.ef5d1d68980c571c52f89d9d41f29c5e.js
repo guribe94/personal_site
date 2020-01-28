ecddad5b5ca756f31b02b3591eca44b7544148ea
Gabriel <vegtam@MacBook.local>
2020-01-28 13:56:27 -0800

    Updates

---
+++ precache-manifest.ef5d1d68980c571c52f89d9d41f29c5e.js
@@ -0,0 +1,138 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f194d398e97f5fc22debb4d16c85e7f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "71fa9a8c58a1b1566de6",
+    "url": "/static/css/2.c9e9a78e.chunk.css"
+  },
+  {
+    "revision": "2835c843f5a70e1a026e",
+    "url": "/static/css/main.82cc53cc.chunk.css"
+  },
+  {
+    "revision": "71fa9a8c58a1b1566de6",
+    "url": "/static/js/2.9928f2c3.chunk.js"
+  },
+  {
+    "revision": "2835c843f5a70e1a026e",
+    "url": "/static/js/main.73268292.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "e69e37bd6f117ddb67d744c33716a2a9",
+    "url": "/static/media/Resume.e69e37bd.pdf"
+  },
+  {
+    "revision": "43f62b103c33f53ea8becff3f1725ac6",
+    "url": "/static/media/Vmarine.43f62b10.jpg"
+  },
+  {
+    "revision": "d7b4a5cb993a4a88ce3f96ecf2477b73",
+    "url": "/static/media/aguaDeLuz-1.d7b4a5cb.png"
+  },
+  {
+    "revision": "427cf2c0c4093d111e3270f375040b0b",
+    "url": "/static/media/aguaDeLuz-2.427cf2c0.png"
+  },
+  {
+    "revision": "d3a2220fa7c91b332ba1878195aff059",
+    "url": "/static/media/aguaDeLuz-3.d3a2220f.png"
+  },
+  {
+    "revision": "aaccde7267457b089b51c7e2096e39dd",
+    "url": "/static/media/aguaDeLuz-4.aaccde72.png"
+  },
+  {
+    "revision": "851ac5cd41c1272715245e19eb765cce",
+    "url": "/static/media/aguaDeLuz.851ac5cd.png"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "05acfdb568b3df49ad31355b19495d4a",
+    "url": "/static/media/ionicons.05acfdb5.woff"
+  },
+  {
+    "revision": "24712f6c47821394fba7942fbb52c3b2",
+    "url": "/static/media/ionicons.24712f6c.ttf"
+  },
+  {
+    "revision": "2c2ae068be3b089e0a5b59abb1831550",
+    "url": "/static/media/ionicons.2c2ae068.eot"
+  },
+  {
+    "revision": "621bd386841f74e0053cb8e67f8a0604",
+    "url": "/static/media/ionicons.621bd386.svg"
+  },
+  {
+    "revision": "5b74904d73a82ad81d40dba23d12ce40",
+    "url": "/static/media/northern_light.5b74904d.jpg"
+  },
+  {
+    "revision": "7f62f36991d2fdf7e9ee668156ee0d16",
+    "url": "/static/media/person.7f62f369.svg"
+  },
+  {
+    "revision": "66ee3e33ad39b3bea037223e540db558",
+    "url": "/static/media/todo-1.66ee3e33.png"
+  },
+  {
+    "revision": "048ab149bd392c063c187556b2583b24",
+    "url": "/static/media/todo-2.048ab149.png"
+  },
+  {
+    "revision": "7282f3f47663696d172857d2a8467b49",
+    "url": "/static/media/todo-3.7282f3f4.png"
+  },
+  {
+    "revision": "ac82d97d170d742395675707f640e98d",
+    "url": "/static/media/todolist.ac82d97d.png"
+  },
+  {
+    "revision": "4cf363f8f9a0fad82f3397e3b88e7a75",
+    "url": "/static/media/vmarine-1.4cf363f8.png"
+  },
+  {
+    "revision": "ce05e1d947986ea5db8cdc20a888d09f",
+    "url": "/static/media/vmarine-2.ce05e1d9.png"
+  },
+  {
+    "revision": "df3dafe0c1e793e5697303556bf88884",
+    "url": "/static/media/vmarine-3.df3dafe0.png"
+  },
+  {
+    "revision": "8db57d5a31d63446801b3885a57c79f3",
+    "url": "/static/media/vmarine-5.8db57d5a.png"
+  },
+  {
+    "revision": "6e2642f8f10d6c00c86c28de0be68725",
+    "url": "/static/media/vmarine-6.6e2642f8.png"
+  },
+  {
+    "revision": "a16ea18c42061c568f85f0677570d157",
+    "url": "/static/media/vmarine-7.a16ea18c.png"
+  }
+]);
